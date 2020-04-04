4f458e5c41d248485329de6a9d87715f36c98c88
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-03 23:05:26 -0600

    Add alerts and control sessions to hello.html

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -50,7 +50,7 @@ def hello():
     if form.validate_on_submit():
         username = form.username.data
         session['username'] = username
-
+        flash('Nombre de usuario registrado con éxito')
         return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
