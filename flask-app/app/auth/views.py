8fd0b45e1617864e9d751d401147e10b480a0894
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-04 17:16:44 -0600

    :sparkles: Auth users with SignUp and Logout

--- flask-app/app/auth/views.py
+++ flask-app/app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, url_for, session, flash
 from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash
 from app.forms import LoginForm
 from . import auth
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.models import UserModel, UserData
 
 @auth.route('/login', methods=['GET','POST'])
@@ -35,6 +36,35 @@ def login():
         
         return redirect(url_for('index'))
     return render_template('login.html', **context)
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    form = LoginForm()
+    context = {
+        'signup_form':form
+    }
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+            login_user(user)
+            
+            flash('Bienvenido')
+            return redirect(url_for('hello'))
+        else:
+            flash('El usuario ya existe')
+
+    return render_template('signup.html', **context)
 
 @auth.route('logout')
 @login_required
--- flask-app/app/firestore_service.py
+++ flask-app/app/firestore_service.py
@@ -13,6 +13,9 @@ def get_users():
 def get_user(user_id):
     return db.collection('users').document(user_id).get()
 
-
 def get_todos(user_id):
     return db.collection('users').document(user_id).collection('todos').get()
+
+def user_put(user_data):
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
