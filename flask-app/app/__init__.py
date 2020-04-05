52640e80eac98cafe6465902006e28f440868872
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-04 21:39:33 -0600

    :art: Upgrade project to Bootstrap 4

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -7,6 +7,7 @@ from .models import UserModel
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+login_manager.login_message = u"Inicia sesión para acceder a está página."
 
 @login_manager.user_loader
 def load_user(username):
--- flask-app/app/firestore_service.py
+++ flask-app/app/firestore_service.py
@@ -28,7 +28,7 @@ def user_put(user_data):
 def put_todo(user_id, description):
     todos_collection_ref = db.collection(
         'users').document(user_id).collection('todos')
-    todos_collection_ref.add({'description': description})
+    todos_collection_ref.add({'description': description, 'done': False})
 
 def delete_todo(user_id, todo_id):
     todo_ref = __get_todo_ref(user_id, todo_id)
